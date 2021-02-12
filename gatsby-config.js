ee4dcaf6d3f730883f8caab313e5d59284e8b7ef
Andrew Weiss <wandrew8@gmail.com>
2021-02-12 07:47:18 -0800

    adds blog template and routing to markdown pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
 module.exports = {
+  pathPrefix: "/blog",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
