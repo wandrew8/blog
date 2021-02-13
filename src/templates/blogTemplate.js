ba79b2385e2f3f24936233090ddca7b2e146402d
Andrew Weiss <wandrew8@gmail.com>
2021-02-12 20:24:11 -0800

    adds useMediaQuery hook for styling and conditional rendering of blog template

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -3,15 +3,16 @@ import { Helmet } from "react-helmet"
 import { graphql } from "gatsby"
 import styled from "styled-components"
 import AuthorAside from '../components/authorAside'
-
+import { devices } from '../styles/devices'
+import useMediaQuery from '../hooks/mediaQuery'
 export default function Template({ data }) {
   const { markdownRemark: post } = data
-  console.log(post.frontmatter.authorImage)
+  let isPageWide = useMediaQuery(devices.tablet);
   return (
     <PageContainer>
       <Helmet title={`${post.frontmatter.title}`} />
       <Container>
-        <AuthorAside authorBio={post.frontmatter.authorBio} author={post.frontmatter.author} authorImage={post.frontmatter.authorImage.childImageSharp.fluid}/>
+        {isPageWide && <AuthorAside mobile={isPageWide} authorBio={post.frontmatter.authorBio} author={post.frontmatter.author} authorImage={post.frontmatter.authorImage.childImageSharp.fluid}/> }
         <div className="blog-post">
           <h1>{post.frontmatter.title}</h1>
           <div
@@ -51,10 +52,22 @@ const Container = styled.div`
   margin: 2rem auto;
   text-align: left;
   display: grid;
-  grid-template-columns: 200px 4fr;
+  grid-template-columns: 1fr;
   grid-gap: 2rem;
   position: relative;
+  @media ${devices.tablet} { 
+    grid-template-columns: 200px 4fr;
+  }
 `
 const PageContainer = styled.div`
-  margin: 6rem;
+  margin: 2rem;
+  @media ${devices.mobileL} { 
+    margin: 4rem;
+  }
+  @media ${devices.tablet} { 
+    margin: 6rem 4rem;
+  }
+  @media ${devices.laptop} { 
+    margin: 6rem;
+  }
 `;
